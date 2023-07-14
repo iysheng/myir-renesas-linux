93ad978a54a4a381097c5d63334809532b9068a6
iysheng <iysheng@163.com>
2023-07-14 16:53:08 +0800

    [CHG][N/T] 1. 修改导出的 dts 名称 2. 优化脚本 help 打印信息

--- red_build.sh
+++ red_build.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
 
+function do_build
+{
+    make ARCH=arm64 Image dtbs -j2
+}
+
+function do_menuconfig
+{
+    make ARCH=arm64 menuconfig
+}
+
+function do_dtb2dts
+{
+    dtc -I dtb -O dts arch/arm64/boot/dts/myir/myb-rzg2l-disp.dtb > myb-rzg2l-disp.dts
+}
+
+function do_genpatch
+{
+	git diff > ~/Codes_of_pro/blackmagic4RZ/misc/linux_kernel_Documents.diff;
+	cp cscope.files ~/Codes_of_pro/blackmagic4RZ/misc/kernel_cscope.files;
+}
+
+function do_mergepatch
+{
+	patch -i ~/Codes_of_pro/blackmagic4RZ/misc/linux_kernel_Documents.diff -p 1;
+}
+
 function do_copy
 {
 	echo "copy Image and dtb files to ../build"
@@ -8,25 +34,31 @@ function do_copy
 	fd -e dtb --exclude *rzg2ul* . arch/arm64/boot/dts/myir -IH -X cp -frv {} ../build
 }
 
+function do_help
+{
+    echo "===============help message================="
+	echo "b: do build"
+	echo "c: do menuconfig"
+	echo "d: do dtb2dts"
+	#echo "e: do menuconfig"
+	echo "g: do genpatch"
+	echo "m: do mergepatch"
+	echo "u: do copy build targets"
+}
+
 if [ $# -eq 0 ];then
 # 编译内核文件, 生成 Image 和 dtb 文件
-make ARCH=arm64 Image dtbs -j16
+echo "I thought you shouldn't ignore paramter, so do nothing for blank paramter"
 else
 case $1
 	in
-	c) echo "do menuconfig";make ARCH=arm64 menuconfig;;
-	d) echo "dtb->dts"; dtc -I dtb -O dts arch/arm64/boot/dts/myir/myb-rzg2l-disp.dtb > myir_rzg2.dts;;
-	e) echo "init enviroment"; export PATH=/usr/bin:$PATH; unset LD_LIBRARY_PATH; source ../../03_Tools/Toolchains/sdk/core_red/environment-setup-aarch64-poky-linux;;
-	g)
-	# gen patch
-	echo "gen patch";
-	git diff > ~/Codes_of_pro/blackmagic4RZ/misc/linux_kernel_Documents.diff;
-	cp cscope.files ~/Codes_of_pro/blackmagic4RZ/misc/kernel_cscope.files;;
-	m)
-	# merge patch
-	echo "merge patch";
-	patch -i ~/Codes_of_pro/blackmagic4RZ/misc/linux_kernel_Documents.diff -p 1;;
+	b) do_build;;
+	c) do_menuconfig;;
+	d) do_dtb2dts;;
+	#e) echo "init enviroment"; export PATH=/usr/bin:$PATH; unset LD_LIBRARY_PATH; source ../../03_Tools/Toolchains/sdk/core_red/environment-setup-aarch64-poky-linux;;
+	g) do_genpatch;;
+	#m) do_mergepatch;;
 	u) do_copy;;
-	*) echo "No support this param $1 [g: gen patch] [m: merge patch]";;
+	*) echo "No support this param $1";do_help;;
 esac
 fi
